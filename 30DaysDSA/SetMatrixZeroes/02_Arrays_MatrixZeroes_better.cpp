0c710a152be274b96a20bd74fa96246d4292e784
Aniket Kumar <aa6443@srmist.edu.in>
2025-01-11 06:29:51 +0530

    Better Solution for set matrix zeroes

--- 30DaysDSA/SetMatrixZeroes/02_Arrays_MatrixZeroes_better.cpp
+++ 30DaysDSA/SetMatrixZeroes/02_Arrays_MatrixZeroes_better.cpp
@@ -2,7 +2,33 @@
 using namespace std;
 
 int main(){
-    
+    vector<vector<int>> matrix = {{1,1,1},{1,0,1},{1,1,1}};
+    int n = matrix.size();
+    //taking two extra arrays and initialising it with zero 
+    vector<int> row(n,0);
+    vector<int> col(n,0);
+    // travelling to find zeroes and marking it 
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(matrix[i][j] == 0){
+                row[i] = 1;
+                col[j] = 1;
+            }
+        }
+    }
+    //again traversing to make initial array zero 
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(row[i] == 1 || col[j] == 1) matrix[i][j] = 0;
+        }
+    }
+    //printing matrix 
+    for(auto it:matrix){
+        for(auto i:it){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
 
     return 0;
 }
